dbfe127c8ae737440deeea79227418558d6444fd
Sharan Kumar Reddy <dropmailtosharan1@gmail.com>
2024-04-16 10:03:44 +0530

    first commit

---
+++ frontend/js/app.js
@@ -0,0 +1,48 @@
+// sidebar functionality
+(function () {
+  const hamburger = document.querySelector(".hamburger");
+  const sidebarCloseButton = document.querySelector(".close-button");
+  const sidebarContainer = document.querySelector(".sidebar-container");
+
+  // Dark mode checkbox
+  const checkbox = document.getElementById("checkbox");
+  checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+  });
+
+  const updateAccessiblityChecks = function (sidebarOpening) {
+    const sidebarLinks = [
+      ...document.querySelectorAll(".sidebar-links .nav-link"),
+    ];
+    sidebarLinks.forEach((eachLink) => {
+      eachLink.setAttribute("tabIndex", sidebarOpening ? 0 : -1);
+    });
+    document
+      .querySelector(".sidebar-container .logo-section a")
+      .setAttribute("tabIndex", sidebarOpening ? 0 : -1);
+  };
+  const openSidebar = function () {
+    sidebarContainer.classList.add("show-sidebar");
+    updateAccessiblityChecks(true);
+  };
+  const closeSidebar = function () {
+    sidebarContainer.classList.remove("show-sidebar");
+    updateAccessiblityChecks(false);
+  };
+  hamburger.addEventListener("click", openSidebar);
+  sidebarCloseButton.addEventListener("click", closeSidebar);
+  updateAccessiblityChecks(false);
+})();
+
+// sticky sidebar
+(function () {
+  const navbarContainer = document.querySelector(".nav-container");
+  window.addEventListener("scroll", function () {
+    let targetNumber = this.innerWidth >= 992 ? 70 : 50;
+    if (this.scrollY >= targetNumber) {
+      navbarContainer.classList.add("sticky-nav");
+    } else {
+      navbarContainer.classList.remove("sticky-nav");
+    }
+  });
+})();
